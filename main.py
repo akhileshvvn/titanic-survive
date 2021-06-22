878ae92962c833591bc1f3513aaade7044af90b1
Akhilesh VVN <57211919+akhileshvvn@users.noreply.github.com>
2021-06-22 17:31:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -94,7 +94,7 @@ def test():
         return render_template("home2.html",survive=str((predictions.tolist()[0][1])*100)+"%")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0')
 
 
 
